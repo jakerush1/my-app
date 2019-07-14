6362f88c255d790ebb19c8c9d238ad9816649d86
Jake Rush <jake@toneden.io>
2019-07-14 14:58:09 -0700

    New files.

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+    <div>
+        <div className='Hero'>
+            <div className='HeroGroup'>
+                <h1>Learn to design and code React apps</h1>
+                <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+                <Link to="/page-2/">Watch the video</Link>
+            </div>
+        </div>
+    </div>  
+)
+
+export default IndexPage
